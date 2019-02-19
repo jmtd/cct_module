1c0e1f8b9bdedcf7733cba8c82d740cbca7e7981
Jonathan Dowland <jdowland@redhat.com>
2019-02-19 15:47:34 +0000

    remove KIE material from cct_modules

--- keycloak-layer/configure_layer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-ADDED_DIR=${SCRIPT_DIR}/added
-
-cp -p "$ADDED_DIR/modules/layers.conf" "$JBOSS_HOME/modules/"
-chown jboss:root $JBOSS_HOME/modules/layers.conf
-chmod g+rwX $JBOSS_HOME/modules/layers.conf
