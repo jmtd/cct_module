e65fe3afdd3f360f35681950330519eea0741016
Jonathan Dowland <jdowland@redhat.com>
2019-11-09 21:57:13 +0000

    Initial move to RPM for Prometheus JAR
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- jboss/container/prometheus/bash/configure.sh
+++ jboss/container/prometheus/bash/configure.sh
@@ -5,10 +5,7 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
-mv /tmp/artifacts/jmx_prometheus_javaagent-*.jar ${ARTIFACTS_DIR}/opt/jboss/container/prometheus
-
 chown -R jboss:root ${ARTIFACTS_DIR}
-chmod 444 ${ARTIFACTS_DIR}/opt/jboss/container/prometheus/jmx_prometheus_javaagent-*.jar
 chmod 755 ${ARTIFACTS_DIR}/opt/jboss/container/prometheus/prometheus-opts
 chmod 775 ${ARTIFACTS_DIR}/opt/jboss/container/prometheus/etc
 chmod 775 ${ARTIFACTS_DIR}/opt/jboss/container/prometheus/etc/jmx-exporter-config.yaml
@@ -16,6 +13,3 @@ chmod 775 ${ARTIFACTS_DIR}/opt/jboss/container/prometheus/etc/jmx-exporter-confi
 pushd ${ARTIFACTS_DIR}
 cp -pr * /
 popd
-
-ln -s /opt/jboss/container/prometheus/jmx_prometheus_javaagent-*.jar /opt/jboss/container/prometheus/jmx_prometheus_javaagent.jar
-chown -h jboss:root /opt/jboss/container/prometheus/jmx_prometheus_javaagent.jar
